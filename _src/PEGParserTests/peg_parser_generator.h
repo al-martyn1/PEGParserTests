2f992d9d58099474924badbea692e8fcf6d22cea
al-martyn1 <amart@mail.ru>
2025-02-01 21:10:52 +0300

    Added HAS_UMBA control macro

--- _src/PEGParserTests/peg_parser_generator.h
+++ _src/PEGParserTests/peg_parser_generator.h
@@ -1,14 +1,23 @@
 #pragma once
 
-#include "umba/warnings/push.h"
+#if defined(HAS_UMBA)
 
-    #include "umba/warnings/disable_C4244.h"
-    #include "umba/warnings/disable_C4267.h"
-    #include "umba/warnings/disable_C4365.h"
-    #include "umba/warnings/disable_C4868.h"
-    #include "umba/warnings/disable_C5219.h"
+    #include "umba/warnings/push.h"
+    
+        #include "umba/warnings/disable_C4244.h"
+        #include "umba/warnings/disable_C4267.h"
+        #include "umba/warnings/disable_C4365.h"
+        #include "umba/warnings/disable_C4868.h"
+        #include "umba/warnings/disable_C5219.h"
+    
+        #include <peg_parser/generator.h>
+    
+    #include "umba/warnings/pop.h"
 
-    #include <peg_parser/generator.h>
+#else
 
-#include "umba/warnings/pop.h"
+    // No UMBA
 
+    #include "umba/warnings/pop.h"
+
+#endif
